2eb6a5d622b1c8911ace706d6dfc1a14f71404ab
ars <svetarsbrest@gmail.com>
2023-05-28 12:37:13 +0300

    buf commit just in case

--- calculate_stat.py
+++ calculate_stat.py
@@ -1,50 +1,18 @@
-import os
-import numpy as np
 from termgraph import termgraph as tg
-from datetime import datetime, timedelta
+from db import Database
 
 def calculate_stat(start, end, url):
-    files = []
-    path = f'saves/'
+    db = Database()
 
-    needed = url.split('/')[-1]
-    for dirname in os.listdir(path):
-        if needed not in dirname:
-            continue
-        dir_path = os.path.join(path, dirname + "/labels")
-        for filename in os.listdir(dir_path):
-            file_path = os.path.join(dir_path, filename)
-            if os.path.isfile(file_path):
-                create_time = datetime.fromtimestamp(os.path.getctime(file_path))
-                if start <= create_time <= end:
-                    files.append(file_path)
-
-    files = sorted(files, key=lambda x: int(x.split('_')[-1].split('.')[0]))
-
-    d = {}
-    for f in files:
-        with open(f, "r", encoding="utf-8") as infile:
-            for line in infile:
-                line = line.replace("\n", "").split(" ")
-
-                id = line[-1] if len(line) == 6 else None
-                if not id: continue
-
-                cls = line[0]
-                time = datetime.fromtimestamp(os.path.getctime(f))
-                if id in d:
-                    d[id]["time"] = time - d[id]["time_in"]
-                else:
-                    d[id] = {"cls": cls, "time_in": time, "time": timedelta(days=0, seconds=0, microseconds=0)}
-
+    records = db.get_records(url.split('/')[-1], start, end)
 
     class_durations = {"0": [], "1": [], "2": [], "3": [], "5": [], "7": []}
 
-    for id in d.keys():
-        cls = d[id]["cls"]
-        if cls in class_durations.keys(): 
-            duration = d[id]["time"].total_seconds()
-            class_durations[cls].append(duration)
+    for cls, time in records:
+        if cls in class_durations:
+            class_durations[cls].append(float(time))
+
+    db.close()
 
     person = class_durations["0"]
     bicycle = class_durations["1"]
@@ -83,16 +51,5 @@ def plot(labels, data, normal_data):
     colors = [91, 92, 93, 94, 95, 96]
     tg.print_categories(["person", "bicycle", "car", "motorcycle", "bus", "truck"], colors)
     tg.stacked_graph(labels, data, normal_data, len_categories, args, colors)
-
-
-if __name__ == "__main__":
-    start = datetime(2023, 5, 1)
-    end = datetime(2023, 5, 25)
-    url = "https://s1.moidom-stream.ru/s/public/0000001301.m3u8"    
-
-    labels = get_labels()
-    data = get_data(*calculate_stat(start, end, url))
-    
-    plot(labels, data, tg.normalize(np.log1p(data),25))
 
 
